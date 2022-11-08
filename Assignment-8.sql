74db43930f85ed3307043adedcdf8cde076d8eda
Sibayan Misra <sibayanmisra2002@gmail.com>
2022-11-08 22:00:07 +0530

    Update Assignment-8.sql

--- Assignment-8.sql
+++ Assignment-8.sql
@@ -1,11 +1,20 @@
 use assignment_8;
-1)iii)
-select * from author1 where author1_id in (select author_id from catalogue1 where year>2000 and price>(select avg(price) from catalogue1) group by author1_id having count(*)>1;
-iv)
-select author1_name from author1 a.catalogue1 e where a.author1_id=e.author1_id and book_id in (select book_id from order details 1 where quantity=(select max(quantity) from order details1);
-v)
-update catalogue1 set price=1.1* price where publisher1_id in (select publisher1_id from publisher1 where publisher1_name='person');
-2)iii)
+
+1)
+i) CREATE TABLE Author (author_id number(10), author_name varchar2(20), author_city varchar2(20), author_country varchar2(20),
+     primary key(author_id));
+
+ii) insert into Author values('&author_id','&author_name','&author_city','&author_country');
+
+iii) select * from author1 where author1_id in (select author_id from catalogue1 where year>2000 and price>(select avg(price) from catalogue1) group by author1_id having count(*)>1;
+
+iv) select author1_name from author1 a.catalogue1 e where a.author1_id=e.author1_id and book_id in (select book_id from order details 1 where quantity=(select max(quantity) from order details1);
+
+v) update catalogue1 set price=1.1* price where publisher1_id in (select publisher1_id from publisher1 where publisher1_name='person');
+
+2)
+
+iii)
 select customer_name from depositor where acc_no from account branch_name='main') group by acc_no having count(acceno)>2;
 iv)
 select customer_name from customer where acc_no in  (select acc_no from account where branch_name in (select branch_name from branch where branch_city like 'Bangalore'));
